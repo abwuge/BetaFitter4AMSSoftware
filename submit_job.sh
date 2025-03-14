1c4da56d134b15ca929c49530709e19a9136e6fe
Hanxun Wu <abwuge@163.com>
2025-03-14 11:25:44 +0800

    [Improv]: ⚡ Streamline job submission with parameterized script
    - Create wrapper script for HTCondor job submission
    - Add support for customizing Z, fitOption and energyLossScale parameters
    - Remove hardcoded values in favor of command-line arguments
    - Set sensible defaults for all parameters
    - ⚛️ Now your particles can be submitted with quantum efficiency

---
+++ submit_job.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+Z=${1:-8}
+fitOption=${2:-0}
+energyLossScale=${3:-2.0}
+
+condor_submit submit.sub -define "Z=${Z}" -define "fitOption=${fitOption}" -define "energyLossScale=${energyLossScale}"
